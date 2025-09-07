6d15a347eb0d02d46925d9e00634c3f18db28b9f
AaronRM2003 <133229722+AaronRM2003@users.noreply.github.com>
2025-09-07 18:09:23 +0530

    Stage and commit deleted files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 5173
-  }
-})
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 5173,
+    },
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: mode === 'production',  // only drop logs in production
+          drop_debugger: mode === 'production',
+        },
+      },
+    },
+  };
+});
